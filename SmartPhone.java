01328a2423eead32d3c428f8a8bc45d2b19bf0c1
charliembrodie <cbrodi01@cspc061.dcs.bbk.ac.uk>
2012-11-05 16:27:10 +0000

    The extension of mobilePhone.java, midway through exercise 3, full functionality on exercises 1 & 2.

---
+++ SmartPhone.java
@@ -0,0 +1,25 @@
+public class SmartPhone extends MobilePhone{
+    private String position = "0238738478, 34728934729384";
+
+    public SmartPhone(){
+	}
+	
+	@Override
+	public void call(String number){
+	    if(number.charAt(0) == '0' && number.charAt(1) == '0'){
+		    System.out.println("Calling " + number + " throught the internet to save money");
+		} else {
+		    super.call(number);
+		}
+	}
+	
+    public void browseWeb(String Address){
+	    System.out.println("Now searching for " + Address);
+	}
+	
+	public String findPosition(){
+	    System.out.println("Your location is " + position);
+	    return position;
+	}
+	
+}
