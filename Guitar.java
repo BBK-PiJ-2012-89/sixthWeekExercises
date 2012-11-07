d36647139419081a1fca97cc31d93f705c28725a
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-11-07 15:46:00 +0000

    I have now completed this exercise (Exercise 5), full functionality.

---
+++ Guitar.java
@@ -0,0 +1,15 @@
+public class Guitar extends MusicalInstrument implements WoodenObject {
+
+    public Guitar(){
+
+    }
+    
+    public void burn(){
+
+    }
+    /**
+    *
+    * As java does not allow mutliple inheritance via the extension of more than one class,
+    * instead we must implement wooden object as an interface rather than its own class
+    */
+}
