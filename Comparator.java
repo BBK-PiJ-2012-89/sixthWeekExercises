a57d9ea2e2cd0a750145e1bb18fb4198af6fab29
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-11-14 09:51:45 +0000

    I have now completed exercise 1, full functionality.

---
+++ Comparator.java
@@ -0,0 +1,24 @@
+public class Comparator {
+
+	public int getMax(int n, int m){
+        double n1 = (double) n;
+	    double n2 = (double) m;
+	    int ans = ((int) getMax(n1, n2));
+	    return ans;
+	}
+
+	public double getMax(double d1, double d2){
+	    if(d1 > d2){
+	        return d1;
+	    } else {
+	        return d2;
+	    }
+	}
+	public String getMax(String number1, String number2){
+	    double n1 = Double.parseDouble(number1);
+	    double n2 = Double.parseDouble(number2);
+	    String ans = Double.toString(getMax(n1, n2));
+	    return ans;
+
+	}
+}
